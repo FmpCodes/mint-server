42a567b2f4020d32e5dc4216384542fa79846035
FmpCodes <38545672+FmpCodes@users.noreply.github.com>
2020-11-15 12:37:42 +0100

    Update mint.py

--- mint.py
+++ mint.py
@@ -4,5 +4,9 @@ import socket
 HOST, PORT = '', 8888
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 listen_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
+listen_socket.bind((HOST,PORT))
+
+listen_socket.listen(1)
