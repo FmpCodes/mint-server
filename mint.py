85a1dcd9e3ed79fb8769b6773805f5b3362cd431
FmpCodes <akorsunday070@gmail.com>
2023-04-02 10:13:18 +0100

    updated mint

--- mint.py
+++ mint.py
@@ -1,7 +1,9 @@
 # Using Python 3.xx
 import socket
 
+
 HOST, PORT = '', 8888
+
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,7 +13,9 @@ listen_socket.bind((HOST,PORT))
 
 listen_socket.listen(1)
 
+
 print(f'Serving HTTP on port {PORT} ...')
+
 while True:
   client_connection, client_address = listen_socket.accept()
   request_data = client_connection.recv(1024)
@@ -25,3 +29,8 @@ Hello, World!
   client_connection.sendall(http_response)
   client_connection.close()
 
+#Code Explanation
+
+#Line 2 : The first line "import socket" is simply instructing the python interpreter to add or make visible the socket module of python which contains useful methods for executing an instance of a client<->server request-response cycle. 
+
+#Line 4 : The values on the right are assigned the values on the left
